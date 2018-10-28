0cfcd7a797187a12567143a173efe6afc7e8875e
tpage99 <taylorvtx1300@gmail.com>
2018-10-27 21:07:16 -0500

    added resolver function to include links

--- src/index.js
+++ src/index.js
@@ -4,13 +4,31 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
 //2
 const resolvers = {
     Query: {
-        info: () => 'This is the API of a Hackernews clone'
+        info: () => 'This is the API of a Hackernews Clone',
+        feed: () => links,
+    },
+    Link: {
+        id: (root) => root.id,
+        description: (root) => root.description,
+        url: (root) => root.url,
     }
 }
 
