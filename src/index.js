0903959cf701f5ff4805de6a030625fddc9dfaf8
tpage99 <taylorvtx1300@gmail.com>
2018-10-29 22:12:22 -0500

    learning more about git and pushing to github

--- src/index.js
+++ src/index.js
@@ -1,40 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-//1
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-//2
 const resolvers = {
     Query: {
         info: () => 'This is the API of a Hackernews Clone',
-        feed: () => links,
+        feed: () => (root, args, context, info) => {
+            return context.db.query.links({}, info)
+        },
     },
-    Link: {
-        id: (root) => root.id,
-        description: (root) => root.description,
-        url: (root) => root.url,
-    }
+    Mutation: {
+        post: (root, args, context, info) => {
+            return context.db.mutation.createLink({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            }, info)
+        },
+    },
 }
 
-//3
+//3 added comment
 const server = new GraphQLServer ({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
+ 
 server.start(() => console.log('Server is up and running on http://localhost:4000'))
