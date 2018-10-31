db5562ac464c0867619157e0b511b24d08175610
tpage99 <taylorvtx1300@gmail.com>
2018-10-30 21:37:04 -0500

    re-updated info from 10/29/2018

--- src/index.js
+++ src/index.js
@@ -3,38 +3,55 @@ const { GraphQLServer } = require('graphql-yoga')
 //1
 const typeDefs = `
 type Query {
-    info: String!
-    feed: [Link!]!
+  info: String!
+  feed: [Link!]!
+}
+
+type Mutation {
+  post(url: String!, description: String!): Link!
 }
 
 type Link {
-    id: ID!
-    description: String!
-    url: String!
+  id: ID!
+  description: String!
+  url: String!
 }
 `
+
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
 }]
 
-//2
-const resolvers = {
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+ 
+  const resolvers = {
     Query: {
-        info: () => 'This is the API of a Hackernews Clone',
-        feed: () => links,
+      info: () => `This is the API of a Hackernews Clone`,
+      feed: (root, args, context, info) => {
+        return context.db.query.links({}, info)
+      },
     },
-    Link: {
-        id: (root) => root.id,
-        description: (root) => root.description,
-        url: (root) => root.url,
-    }
-}
+    Mutation: {
+      post: (root, args, context, info) => {
+        return context.db.mutation.createLink({
+          data: {
+            url: args.url,
+            description: args.description,
+          },
+        }, info)
+      },
+    },
+  }
 
-//3
 const server = new GraphQLServer ({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
+
 server.start(() => console.log('Server is up and running on http://localhost:4000'))
